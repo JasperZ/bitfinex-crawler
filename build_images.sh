c9d7cf749facd0c986f192ddc2cb327dbecb3ac9
JasperZ <jasper.android@gmail.com>
2019-02-26 17:04:27 +0100

    - removed cd

--- build_images.sh
+++ build_images.sh
@@ -3,9 +3,7 @@
 VERSION="0.10.0"
 
 # build image
-cd docker
 docker build -t $DOCKER_USERNAME/bitfinex-crawler:$VERSION -f docker/Dockerfile .
-cd ..
 
 # login to docker hub
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
